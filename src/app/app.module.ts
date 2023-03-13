73c65d6167df616af5b9e0db83add5bca1b3801b
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-03-12 21:19:13 -0500

    Se inicia a trabajar sobre el modulo dbz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-//Tarea:
-// Crear un módulo llamado ContadorModule
-// declaraciones, y exportaciones
 import { ContadorModule } from './contador/contador.module';
 
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -17,8 +15,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    // importar aquí
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
